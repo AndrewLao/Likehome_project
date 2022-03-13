0379f4a0c39da82f8d3ed9a6764acecd842c52c1
Liz <lshuelfenhaus@gmail.com>
2022-03-12 16:57:27 -0800

    Added empty login, signup pages. Added buttons & routes for account, catalog, login, & signup.

---
+++ likehome/src/Account.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function Account() {
+    return (
+        <p>This is the account page</p>
+    );
+}
+
+export default Account;
--- likehome/src/App.js
+++ likehome/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 import React from 'react'
+import './App.css';
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
-import CatalogPage from './CatalogPage';
+import Login from './Login'
+import Account from './Account'
+import SignUp from './SignUp'
+import CatalogPage from './CatalogPage'
 import { BrowserRouter as Router, Switch, Route }
 from "react-router-dom";
 
@@ -14,8 +17,21 @@ function App() {
     <div className="app">
       <Router>
         <Header />
-
+            
         <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route path="/account">
+            <Account />
+          </Route>
+
           <Route path="/catalog">
             <CatalogPage />
           </Route>
