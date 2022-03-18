bfa4dd6d87e18c62f3bf33a95392aba7a8eb7027
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2022-03-18 16:52:33 -0700

    auto update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,12 +9,3 @@ echo installing
 cd /home/ubuntu/appdeploy/
 sudo apt-get update
 
-# # check to make sure the symbolic link for nodejs node exists
-# echo checking for nodejs symlink
-# file="/usr/bin/node"
-# if [ -f $file ] && [ ! -L $file ] ; then
-#   echo "$file exists and is not a symlink"
-#   sudo ln -s /usr/bin/nodejs
-# else
-#   echo "$file exists and is already a symlink"
-# fi
