fabefc61fbc050c0e206d692f0705e569eef2c9c
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2022-03-18 02:17:41 -0700

    fix merge headers

--- likehome/src/App.js
+++ likehome/src/App.js
@@ -1,58 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import './App.css';
-import Home from './Home'
-import Header from './Header'
-import Footer from './Footer'
-import Login from './Login'
-import Account from './Account'
-import SignUp from './SignUp'
-import CatalogPage from './CatalogPage'
-import { BrowserRouter as Router, Switch, Route }
-from "react-router-dom";
-
-function App() {
-  return (
-
-    // BEM
-    <div className="app">
-      <Router>
-        <Header />
-
-        <div className="body">    
-        <Switch>
-
-          <Route path="/login">
-            <Login />
-          </Route>
-
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-
-          <Route path="/account">
-            <Account />
-          </Route>
-
-          <Route path="/catalog">
-            <CatalogPage />
-          </Route>
-          
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        </div>
-
-        <Footer />
-
-      </Router>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react'
 import './App.css';
 import Home from './Home'
@@ -104,4 +49,3 @@ function App() {
 }
 
 export default App;
->>>>>>> Stashed changes
