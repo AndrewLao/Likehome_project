bc74c123e4dd8a1fabd78508bd98ca2b12335c40
AndrewLao <43099770+AndrewLao@users.noreply.github.com>
2022-03-17 23:16:20 -0700

    CI/CD Test

---
+++ likehome/scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Building...
+npm install 
+npm run build
+
--- likehome/src/App.js
+++ likehome/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import './App.css';
 import Home from './Home'
@@ -51,3 +52,56 @@ function App() {
 }
 
 export default App;
+=======
+import React from 'react'
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import Login from './Login'
+import Account from './Account'
+import SignUp from './SignUp'
+import CatalogPage from './CatalogPage'
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <Router>
+        <Header />
+            
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route path="/account">
+            <Account />
+          </Route>
+
+          <Route path="/catalog">
+            <CatalogPage />
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> Stashed changes
