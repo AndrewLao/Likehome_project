ac63c3fd91677b3092712c40368b01074c1959d8
Kenneth <espaillatken12@gmail.com>
2022-03-22 13:05:21 -0700

    Created Login and Sign Up pages

--- likehome/src/Login.js
+++ likehome/src/Login.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Grid, TextField, Button, Typography,Link } from '@material-ui/core'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import "./LoginSignup.css";
+
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const textStyle = {margin:'10px 0px 0px'}
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <p>This is the login page</p>
+        <div className="loginSignup">
+            <Grid align='center'>
+                <h2>Login</h2>
+            </Grid>
+            <TextField label='Email Address' placeholder='Enter email address' variant='outlined' style={textStyle} fullWidth required/>
+            <TextField label='Password' placeholder='Enter password' type='password' variant='outlined' style={textStyle} fullWidth required/>
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                }style={textStyle}
+                label="Remember me"
+            />
+            <Button type='submit' variant="contained" fullWidth>Login</Button>
+            <Typography style={textStyle}>
+                <Link href="./forgotPassword" >
+                    Forgot password?
+                </Link>
+            </Typography>
+            <Typography style={textStyle}> Do you have an account?&nbsp;
+                <Link href="./SignUp" >
+                    Sign Up 
+                </Link>
+            </Typography>
+        </div>
+        
     );
 }
 
--- likehome/src/SignUp.js
+++ likehome/src/SignUp.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Grid, TextField, Button, Typography,Link } from '@material-ui/core'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import './LoginSignup.css';
+
 function SignUp() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const textStyle = {margin:'10px 0px 0px'}
+
+    function validateForm() {
+        return firstName.length > 0 && lastName.length > 0 && email.length > 0 && password.length > 0;
+    }
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <p>This is the sign-up page</p>
+        <div className="loginSignup">
+            <Grid align='center'>
+                <h2>Sign Up</h2>
+            </Grid>
+            <TextField label='First Name' placeholder='Enter first name' variant='outlined' style={textStyle} fullWidth required/>
+            <TextField label='Last Name' placeholder='Enter last name' variant='outlined' style={textStyle} fullWidth required/>
+            <TextField label='Email Address' placeholder='Enter email address' variant='outlined' style={textStyle} fullWidth required/>
+            <TextField label='Password' placeholder='Enter password' type='password' variant='outlined' style={textStyle} fullWidth required/>
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                }style={textStyle}
+                label="Remember me"
+            />
+            <Button type='submit' variant="contained" fullWidth>Sign Up</Button>
+            <Typography style={textStyle}> Already have an account?&nbsp;
+                <Link href="./Login" >
+                    Login
+                </Link>
+            </Typography>
+        </div>
+        
     );
 }
 
