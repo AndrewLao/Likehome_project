aa39eee1f34dc947eddf7d7e72fca2a447e6c607
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2022-04-04 18:35:07 -0700

    edit appspec

--- scripts/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo starting server
-
-# here we just use npm to run the build
-cd /home/ubuntu/appdeploy/
-echo moving build to server path
-cp -r /home/ubuntu/appdeploy/likehome/build/* /var/www/likehome/
